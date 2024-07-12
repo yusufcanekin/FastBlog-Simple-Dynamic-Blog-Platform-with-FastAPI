ebcc3b3b0cdf32c52a697c78ad1f939adc929cd3
Yusuf Can Ekin <yusufcanekin@Yusuf-MacBook-Air-6.local>
2024-07-12 09:39:08 +0300

    before tests

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_create_user():
-    response = client.post("/users", json = {"email":"hello@gmail.com", "password":"password123"})
+    response = client.post("/users/", json = {"email":"halo@gmail.com", "password":"password123"})
     print(response.json())
     assert response.status_code == 201
